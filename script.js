8239286e2faa8690200de3e6c264e25c3c4fc98f
Emanuel Flores <44663300+emanuelflo@users.noreply.github.com>
2020-07-17 04:19:56 -0500

    create about+work pages, refine animations

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const work_page = document.querySelector('#work-page');
+const home = document.querySelector('#home_page');
+const work = document.querySelector('#work_page');
+const about = document.querySelector('#about_page');
+const fade = document.querySelector('.animated');
 
-function workPage() {
-    const main_div = document.querySelector('.main');
-    main_div.classList.add('animated', 'fadeOut');
-    setTimeout(removeNodes, 500);
-    function removeNodes() {
-        while (main_div.firstChild) {
-            main_div.removeChild(main_div.firstChild);
+const varArr = [home, work, about];
+
+
+for (let i = 0; i < varArr.length; i++) {
+    varArr[i].addEventListener('click', (link) => {
+        if (link.srcElement.innerText === 'HOME') {
+            fadeOut();
+            setTimeout(function() {
+                linkOut('index.html');
+            }, 1000);
+        } else if (link.srcElement.innerText === 'WORK') {
+            fadeOut();
+            setTimeout(function() {
+                linkOut('work.html');
+            }, 1000);
+        } else if (link.srcElement.innerText === 'ABOUT') {
+            fadeOut();
+            setTimeout(function() {
+                linkOut('about.html');
+            }, 1000);
         }
-    }
+    })
 }
 
-work_page.addEventListener('click', workPage, false);
+const fadeOut = () => {
+    fade.classList.remove('animated', 'fadeIn');
+    fade.classList.add('animated', 'fadeOut');
+}
+
+function linkOut(page) {
+    window.location.href = "file:///Users/EmanuelFlores/Desktop/Portfolio%20Site%20v2/portfolio_EF/" + page;
+}
